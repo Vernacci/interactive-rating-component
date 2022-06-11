65073c4e8498cc712a1773300162582a5da71f87
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-06-11 14:25:13 -0300

    Fineshed Challenge

--- Js/script.js
+++ Js/script.js
@@ -1,11 +1,26 @@
 const ratingButtons = document.querySelectorAll('.rating-btns button');
 const submitBtn = document.querySelector('.submit-btn');
+const text = document.querySelector('.text');
+const containerStart = document.querySelector('.container-start');
+const ratingStart = document.querySelector('.rating-start');
+const containerEnd = document.querySelector('.container-end');
+const ratingEnd = document.querySelector('.rating-end');
+
+let rating;
 
 ratingButtons.forEach((btn) => {
   btn.addEventListener('click', (e) => {
-    let rating = `You selected ${e.target.value} out of 5`;
+    rating = `You selected ${e.target.value} out of 5`;
     console.log(rating);
     btn.classList.toggle('desativo');
     btn.classList.toggle('ativo');
   });
 });
+
+const handleSubmit = () => {
+  containerStart.classList.add('hide');
+  ratingStart.classList.add('hide');
+  containerEnd.classList.remove('hide');
+  ratingEnd.classList.remove('hide');
+  text.innerText = rating;
+};
