f328abc9d0df836e6553ef2779f9f80c0aebb60d
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-06-10 22:29:58 -0300

    Added hover effects and changed javascript

--- Js/script.js
+++ Js/script.js
@@ -1,9 +1,11 @@
-const ratingButtons = document.querySelectorAll('.rating-btns');
-
-const handleClick = (e) => {
-  console.log(e.target.value);
-};
+const ratingButtons = document.querySelectorAll('.rating-btns button');
+const submitBtn = document.querySelector('.submit-btn');
 
 ratingButtons.forEach((btn) => {
-  btn.addEventListener('click', handleClick);
+  btn.addEventListener('click', (e) => {
+    let rating = `You selected ${e.target.value} out of 5`;
+    console.log(rating);
+    btn.classList.toggle('desativo');
+    btn.classList.toggle('ativo');
+  });
 });
